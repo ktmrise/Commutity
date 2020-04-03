0e5a66f02088a83e25a297dd1ba1f08b3b2e8d20
ktmrise <k2413114403@163.com>
2020-04-03 18:08:44 +0800

    update

---
+++ src/main/java/com/ktm/controller/PublicController.java
@@ -0,0 +1,16 @@
+package com.ktm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublicController {
+    @GetMapping("/publish")
+    public String publish(){
+
+        return "publish";
+
+
+    }
+
+}
